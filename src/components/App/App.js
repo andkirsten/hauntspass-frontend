de989ce478db75cc03b6445cedd527c27d19bbd1
andkirsten <and.kirsten@gmail.com>
2023-09-04 16:16:11 +0900

    tailwind and daisyUI added

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Nav from "../Nav/Nav";
+
+function App() {
+  return;
+  <div className="App">
+    <Nav />
+  </div>;
+}
+
+export default App;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <header>
+        <h1>Daybreak Haunts</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About Us</a>
+            </li>
+            <li>
+              <a href="#services">My Passes</a>
+            </li>
+            <li>
+              <a href="#contact">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Nav;
