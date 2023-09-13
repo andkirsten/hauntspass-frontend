109e26234417b0ea2940c24c05a602b5e5869c8d
andkirsten <and.kirsten@gmail.com>
2023-09-13 09:07:31 +0900

    map added, home header fixed

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Map.css";
 
 const Map = () => {
-  return <div>Map</div>;
+  return (
+    <div>
+      <h1 className="map__title text-center">Haunts Pass Locations</h1>
+      <iframe
+        className="mx-auto d-block map__iframe"
+        src="https://www.google.com/maps/d/embed?mid=1JaUdsXfnYISzAOofIrfnMDYkTWk2d70&ehbc=2E312F"
+        width="640"
+        height="480"
+        title="Haunts Locations Map"
+      ></iframe>
+    </div>
+  );
 };
 
 export default Map;
