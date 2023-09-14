6a7267b42b7b6b7378e206e5bb95799926367912
andkirsten <and.kirsten@gmail.com>
2023-09-14 14:48:53 +0900

    baseurl correction

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://api.hauntspass.ignorelist.com"
+    ? "https://api.daybreakhaunts.ignorelist.com"
     : "http://localhost:3001";
